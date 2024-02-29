a8d98d0b532f821cfec7352645dbacf32bc13d41
Aakash Raj <aakashraj914@gmail.com>
2024-03-01 00:42:06 +0530

    chnaged skills gradient

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -30,54 +30,64 @@ const Skills = () => {
       className="px-2 md:px-4 lg:px-24 mt-20 flex flex-col gap-y-6"
       id="skill"
     >
-      <h2 className="text-4xl md:text-5xl text-center lg:text-start text-white">Skills</h2>
+      <h2 className="text-4xl md:text-5xl text-center lg:text-start text-white">
+        Skills
+      </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 grid-flow-row m-auto md:m-0 gap-4 px-2">
         <div className="main-skills-card">
-          <h3 className="text-white text-3xl py-5">Frontend</h3>
-          <div className="flex gap-4 flex-wrap p-4">
-            {frontend.map((item, index) => {
-              return (
-                <div key={index} className="skills-card">
-                  {item}
-                </div>
-              );
-            })}
+          <div className="bg-black">
+            <h3 className="text-white text-3xl py-5">Frontend</h3>
+            <div className="flex gap-4 flex-wrap p-4">
+              {frontend.map((item, index) => {
+                return (
+                  <div key={index} className="skills-card">
+                    {item}
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
         <div className="main-skills-card">
-          <h3 className="text-white text-3xl py-5">Backend</h3>
-          <div className="flex gap-4 flex-wrap p-4">
-            {backend.map((item, index) => {
-              return (
-                <div key={index} className="skills-card">
-                  {item}
-                </div>
-              );
-            })}
+          <div className="bg-black w-full h-full">
+            <h3 className="text-white text-3xl py-5">Backend</h3>
+            <div className="flex gap-4 flex-wrap p-4">
+              {backend.map((item, index) => {
+                return (
+                  <div key={index} className="skills-card">
+                    {item}
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
         <div className="main-skills-card">
-          <h3 className="text-white text-3xl py-5">Database</h3>
-          <div className="flex gap-4 flex-wrap p-4">
-            {database.map((item, index) => {
-              return (
-                <div key={index} className="skills-card">
-                  {item}
-                </div>
-              );
-            })}
+          <div className="bg-black w-full h-full">
+            <h3 className="text-white text-3xl py-5">Database</h3>
+            <div className="flex gap-4 flex-wrap p-4">
+              {database.map((item, index) => {
+                return (
+                  <div key={index} className="skills-card">
+                    {item}
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
         <div className="main-skills-card">
-          <h3 className="text-white text-3xl py-5">Tools</h3>
-          <div className="flex gap-4 flex-wrap p-4">
-            {tools.map((item, index) => {
-              return (
-                <div key={index} className="skills-card">
-                  {item}
-                </div>
-              );
-            })}
+          <div className="bg-black w-full h-full">
+            <h3 className="text-white text-3xl py-5">Tools</h3>
+            <div className="flex gap-4 flex-wrap p-4">
+              {tools.map((item, index) => {
+                return (
+                  <div key={index} className="skills-card">
+                    {item}
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
