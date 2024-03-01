65f2f40cee7565d97f63e917956a0956ba227951
Aakash Raj <aakashraj914@gmail.com>
2024-03-01 14:57:09 +0530

    fixed mobile nav issues

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -6,24 +6,29 @@ import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 
 const MobileNav = () => {
   return (
-    <div className="md:hidden">
+    <div className="md:hidde">
       <Sheet>
         <SheetTrigger asChild>
-          <Button className="top-4 left-4 absolute bg-black">
-            <Image
-              className="bg-transparent"
-              src="/images/menu_bar.svg"
-              alt="Menu"
-              width={40}
-              height={40}
-            />
-          </Button>
+          <Image
+            className="bg-transparent top-4 left-4 absolute"
+            src="/images/menu_bar.svg"
+            alt="Menu"
+            width={40}
+            height={40}
+          />
         </SheetTrigger>
-        <SheetContent side="left" className="w-[300px] flex flex-col pt-16">
+        <SheetContent
+          side="left"
+          className="w-[300px] flex flex-col pt-16 bg-black border-none"
+        >
           {nav.map((item, index) => {
             return (
-              <Link href={item.href} key={index} className="bg-white">
-                <Button variant="ghost" className="text-xl">
+              <Link
+                href={item.href}
+                key={index}
+                className="bg-black text-white"
+              >
+                <Button variant="ghost" className="text-xl hover:bg-black hover:text-white">
                   {item.name}
                 </Button>
               </Link>
